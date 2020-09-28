52a9532b0fe127bfbc9dc2cc0bc9a539b85fc000
zeidalidiez <69106534+zeidalidiez@users.noreply.github.com>
2020-09-28 00:31:37 -0700

    rehash
    Starting over because I broke the app

--- src/api.js
+++ src/api.js
@@ -2,27 +2,6 @@
 const BASE_URL = 'https://thinkful-list-api.herokuapp.com/zeiddiez';
 
 
-const fetchAPI = function (...args) {
-
-  };
-  
-
-function getItems() {
-   
-}
-
-const createItem = function (item) {
-  
-}
-
-
-const updateItem = function (id, updateData) {
-  
-}
-
-const deleteItem = function (id) {
- 
-}
 
 
 
--- src/app.js
+++ src/app.js
@@ -3,45 +3,6 @@ import $ from 'jquery';
 import store from './store';
 
 
-const pageStart = function(bookmarks) {
-  console.log('pageStart');
-  let menu = getUI();
-  let bookmarkList = getList(bookmarks);
-  return menu + bookmarkList;
-}
-
-const getUI = function() {
-    console.log('getUI');
-  return `<div id='menu'>
-
-
-          </div>`;
-}
-
-const getList = function (bookmarks){
-    console.log('getList');
-  return `<div id='bookmark-list'>
-            ${bookmarks}
-          </div>`;
-}
-
-
-
-const generateNewBookmarkView = function() {
-    console.log('generateNewBookmarkView');
-  let template = `<form action="submit">
-                   
-                  </form>`;
-  return template;
-};
-
-
-
-const handleCreateNewClicked = function() {
-    console.log('handleCreateNewClicked');
- 
-
-}
 
 
 export default {
