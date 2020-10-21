3a0a61500eee910fcd912528e314980dfc1e2278
zeidalidiez <69106534+zeidalidiez@users.noreply.github.com>
2020-10-20 23:27:37 -0700

    changes with mentor

--- src/app.js
+++ src/app.js
@@ -1,37 +1,37 @@
-import $ from "jquery";
-import api from "./api";
-import store from "./store";
-import cuid from "cuid"
+import $ from 'jquery';
+import api from './api';
+import store from './store';
+import cuid from 'cuid';
 
 function startPage() {
   const mainPage = `
-        <div class = "group">
-            <div class = "item">
-                <form id ="add-bookmark">
-                <button type = "submit" class ="btn">New</button>
-                </form>
-            </div>
-            <form id="filter-form">
-            <div class ="item">
-                <select name="filter" class ="btn" id="filter-button">
-                    <option id="filter1" value="1">>1 Stars</option>
-                    <option id="filter2" value="2">>2 Stars</option>
-                    <option id="filter3" value="3">>3 Stars</option>
-                    <option id="filter4" value="4">>4 Stars</option>
-                    <option id="filter5" value="5">>5 Stars</option>
-                </select>
-            </div>
-        <div class="item">
-            <button type="button" id="filter" class = "btn">Filter</button>
-        </div>
+    <div class="group">
+      <div class="item">
+        <form id="add-bookmark">
+          <button type = "submit" class ="btn">New</button>
         </form>
-        </div>`;
+      </div>
+      <form id="filter-form">
+        <div class ="item">
+          <select name="filter" class ="btn" id="filter-button">
+            <option id="filter1" value="1">>1 Stars</option>
+            <option id="filter2" value="2">>2 Stars</option>
+            <option id="filter3" value="3">>3 Stars</option>
+            <option id="filter4" value="4">>4 Stars</option>
+            <option id="filter5" value="5">>5 Stars</option>
+          </select>
+        </div>
+        <div class="item">
+          <button type="button" id="filter" class = "btn">Filter</button>
+        </div>
+      </form>
+    </div>`;
   return mainPage;
 }
 
 function generateBookmarkItem(bookmark) {
-  const bookmarkItem = `<ul>
-  <li class="ind-bookmark" data-item-id="${bookmark.id}"><div class="top-row group"
+  const bookmarkItem = `
+  <li class="ind-bookmark" data-item-id="${bookmark.id}" tabindex="0"><div class="top-row group"
     <div>
     <span class ="item" >${bookmark.title}</span>
       <p class = "item">${bookmark.rating}</p>
@@ -43,7 +43,7 @@ function generateBookmarkItem(bookmark) {
       <button type = "submit" class ="btn "id="delete">Delete</button>
       </div>
     </li>
-    </ul>`;
+    `;
   return bookmarkItem;
 }
 
@@ -55,7 +55,7 @@ function generateBookmarkString(bookmarkList) {
 }
 
 function handleNewButton() {
-  $("main").on("submit", "#add-bookmark", function (e) {
+  $('main').on('submit', '#add-bookmark', function (e) {
     e.preventDefault();
     store.store.adding = true;
     render();
@@ -94,17 +94,17 @@ function generateAddPage() {
 }
 
 function getIdFromElement(item) {
-  return $(item).parent().parent().attr("data-item-id");
+  return $(item).parent().parent().attr('data-item-id');
 }
 
 function handleExpandButton() {
-  $("main").on("click", "li", function (e) {
-    $(this).children(".bottom-row").toggleClass("hidden");
+  $('main').on('click', 'li', function (e) {
+    $(this).children('.bottom-row').toggleClass('hidden');
   });
 }
 
 function handleDeleteButton() {
-  $("main").on("click", "#delete", function (e) {
+  $('main').on('click', '#delete', function (e) {
     e.preventDefault();
     const id = getIdFromElement(e.currentTarget);
     api.deleteBookmark(id).then(() => {
@@ -115,9 +115,9 @@ function handleDeleteButton() {
 }
 
 function handleFilter() {
-  $("main").on("click", "#filter", function (e) {
+  $('main').on('click', '#filter', function (e) {
     e.preventDefault();
-    const filterNum = $("#filter-button").val();
+    const filterNum = $('#filter-button').val();
     store.store.filter = filterNum;
     render();
   });
@@ -130,12 +130,12 @@ function filterBookmarks(bookmarks) {
 }
 
 function handleBookmarkSubmit() {
-  $("main").on("submit", "#add-bookmark-form", function (event) {
+  $('main').on('submit', '#add-bookmark-form', function (event) {
     event.preventDefault();
-    let urlName = $("#url").val();
-    let name = $("#name").val();
-    let rating = $("#starvalue").val();
-    let desc = $("#desc").val();
+    let urlName = $('#url').val();
+    let name = $('#name').val();
+    let rating = $('#starvalue').val();
+    let desc = $('#desc').val();
     api.postBookmark(name, urlName, rating, desc).then((data) => {
       store.addItem(data);
       store.store.adding = false;
@@ -146,13 +146,13 @@ function handleBookmarkSubmit() {
 
 function render() {
   let bookmarksCall = filterBookmarks(store.store.bookmarks);
-  let page = "";
+  let page = '';
   if (store.store.adding === false) {
     page += generateBookmarkString(bookmarksCall);
   } else {
     page += generateAddPage();
   }
-  $("main").html(page);
+  $('main').html(page);
 }
 
 function bindEventListeners() {
